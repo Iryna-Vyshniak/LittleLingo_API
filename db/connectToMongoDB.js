c80ceafc80deb2cd96e58042af326baef0dcf573
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-27 15:47:53 +0300

    feat: Implement MongoDB connection logic with error handling - Use Mongoose for database connection - Retrieve database URI from environment variables - Log connection success or failure messages

--- db/connectToMongoDB.js
+++ db/connectToMongoDB.js
@@ -1,1 +1,16 @@
+import mongoose from 'mongoose';
 
+import env from '../utils/env.js';
+
+const connectToMongoDB = async () => {
+  try {
+    const db = env('MONGODB_URI');
+    await mongoose.connect(db);
+    console.log('Database connection success');
+  } catch (error) {
+    console.log(`Error connect to database ${error.message}`);
+    throw error;
+  }
+};
+
+export default connectToMongoDB;
