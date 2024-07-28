50f5bae6cc64735656504f6a84787f6d7ccda57a
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-28 16:58:43 +0300

    feat: Set up Express server with CORS, routing, and error handling
    - Initialized an Express application and configured it to use CORS with a wildcard origin to allow requests from any source.
    - Parsed incoming JSON requests and URL-encoded data with a size limit of 50MB, enabling the server to handle large form data submissions.
    - Defined a test route `/api/test` to return a welcome message with a status code of 200, providing a basic API endpoint for testing.
    - Integrated `stagesRouter` to handle routes related to the stages data under `/api/stages`.
    - Implemented `missingRouteHandler` middleware to handle undefined routes, providing a standardized response for missing endpoints.
    - Added `globalErrorHandler` middleware to manage errors across the application, ensuring consistent error responses.
    - Set the server to listen on a port defined by an environment variable, with a default fallback, and established a connection to MongoDB on server startup.
    - Exported the Express app instance for further use or testing.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,35 @@
-import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
 
-dotenv.config();
+import env from '../utils/env.js';
+import connectToMongoDB from '../db/connectToMongoDB.js';
+import stagesRouter from '../routes/stage.route.js';
+import missingRouteHandler from '../middlewares/missingRouteHandler.js';
+import globalErrorHandler from '../middlewares/globalErrorHandler.js';
+
+const port = env('PORT', '300');
+const app = express();
+
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+app.use(express.urlencoded({ limit: '50mb', extended: true })); // to parse form data (from req.body)
+
+app.use('/api/test', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'Welcome to the API LittleLingo',
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.use('/api/stages', stagesRouter);
+
+app.use(missingRouteHandler);
+app.use(globalErrorHandler);
+
+app.listen(port, () => {
+  connectToMongoDB();
+  console.log(`Server running on ${port} PORT`);
+});
+
+export default app;
